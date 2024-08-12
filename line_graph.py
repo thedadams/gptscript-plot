892dde720c13891a2b4e861c80387e2de7238831
Donnie Adams <donnie@acorn.io>
2024-08-12 13:39:05 -0400

    fix: use caps for environment variables
    Signed-off-by: Donnie Adams <donnie@acorn.io>

--- line_graph.py
+++ line_graph.py
@@ -3,17 +3,17 @@ import os
 import pandas as pd
 import plotly.express as px
 
-x_label = os.environ.get("x-label", "x")
-y_label = os.environ.get("y-label", "y")
+x_label = os.environ.get("X_LABEL", "x")
+y_label = os.environ.get("Y_LABEL", "y")
 
 df = pd.DataFrame({
-    x_label: [float(x) if x.isdigit() else x for x in os.environ.get("x-values", "").split(",")],
-    y_label: [float(y) if y.isdigit() else y for y in os.environ.get("y-values", "").split(",")]
+    x_label: [float(x) if x.isdigit() else x for x in os.environ.get("X_VALUES", "").split(",")],
+    y_label: [float(y) if y.isdigit() else y for y in os.environ.get("Y_VALUES", "").split(",")]
 })
 
 fig = px.line(
     df, x=x_label, y=y_label,
-    title=os.environ.get("title", "title"),
+    title=os.environ.get("TITLE", "title"),
     markers=True,
 )
 fig.show()
